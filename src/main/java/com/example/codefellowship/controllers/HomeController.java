d59bd238b393c7a6744b988ce5a7bc82b60cfd66
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-08-27 11:39:35 -0700

    Main features complete.

---
+++ src/main/java/com/example/codefellowship/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.codefellowship.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class HomeController
+{
+    @GetMapping("/")
+    public String getRoot(Principal principal, Model model)
+    {
+        model.addAttribute("user", principal);
+        return "root";
+    }
+
+    @GetMapping("/signup")
+    public String getSignup()
+    {
+        return "registration";
+    }
+}
+
