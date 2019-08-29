59cc3b41fb5b021c8b1ad1b7fce57746cb4032c0
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-08-29 13:05:26 -0700

    Fixed the imports.

--- src/main/java/com/example/codefellowship/controllers/PostController.java
+++ src/main/java/com/example/codefellowship/controllers/PostController.java
@@ -4,10 +4,8 @@ import com.example.codefellowship.models.ApplicationUser;
 import com.example.codefellowship.models.ApplicationUserRepository;
 import com.example.codefellowship.models.Post;
 import com.example.codefellowship.models.PostRepository;
-import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
